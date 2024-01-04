d62b2d98910ff5c656b4d608fe1f1556e8f9de0a
artkor37 <artem.korolev037@gmail.com>
2024-01-04 17:11:51 +0200

    Task2.2 done

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-        for(int i=2;i<=100;i=i+2){
+       /* for(int i=2;i<=100;i=i+2){
             System.out.println("Уволен айтишник с id" + i);
         }
 
+        */
+        boolean isId = true;
+        int i = 0;
+        while(isId){
+            if(i==50) {
+                isId=false;
+                break;
+            }
+
+            System.out.println("Уволен айтишник с id " + (i=i+2));
+        }
 
     }
 }
